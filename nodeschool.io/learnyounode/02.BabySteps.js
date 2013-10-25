cc0d4b772ffa28e1cd863e5303963062763bf498
Wojciech Gawronski <afronski@gmail.com>
2013-10-25 09:11:13 +0200

    First 5 lessons from 'learnyounode' tutorial (nodeschool.io FTW).

---
+++ nodeschool.io/learnyounode/02.BabySteps.js
@@ -0,0 +1,1 @@
+console.log(process.argv.slice(2).reduce(function(previous, actual) { return previous + parseInt(actual, 10); }, 0));
---
+++ nodeschool.io/learnyounode/03.MyFirstIO.js
@@ -0,0 +1,6 @@
+var fs = require("fs"),
+    os = require("os"),
+    file = fs.readFileSync(process.argv[2]).toString(),
+    lines = file.split(os.EOL);
+
+console.log(lines.length - 1);
